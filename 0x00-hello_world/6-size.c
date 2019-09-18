748910c1a08b44f3d3bfbbab71f0b4c3aae5e7ee
shincap8 <shincap8@github.com>
2019-09-18 20:40:43 +0000

    Correct task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("Size of a int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a long float: %ld byte(s)\n", size(float));
+	printf("Size of a long float: %ld byte(s)\n", sizeof(float));
 
     	return (0);
 }
