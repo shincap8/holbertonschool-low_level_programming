7ea00e2a531d14c1b385b66f17c89a636350ed01
shincap8 <shincap8@github.com>
2019-09-18 22:00:42 +0000

    Correct task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int B = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", B);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
