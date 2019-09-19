032dc803f85e9fe7ff10a8bc8be8f54753635f8d
shincap8 <shincap8@github.com>
2019-09-19 19:36:36 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Comando puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	printf("\n");
+
+	return (0);
+}
