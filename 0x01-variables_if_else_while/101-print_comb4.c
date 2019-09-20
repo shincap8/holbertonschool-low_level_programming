68cd80395a46ff5cfe58f709aa36e1cb9ae2c280
shincap8 <shincap8@gmail.com>
2019-09-20 00:18:26 +0000

    Task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x <= 55; x++)
+	{
+		for (y = 49; y <= 56; y++)
+		{
+			for (z = 50; z <= 57; z++)
+			{
+				if (x < y && x != y && x != z && y != z && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == 55 && y == 56 && z == 57)
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
