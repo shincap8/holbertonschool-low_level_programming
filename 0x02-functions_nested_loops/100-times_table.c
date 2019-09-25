465327f5713dd72a1dea88680c3826e724a8d64b
shincap8 <shincap8@gmail.com>
2019-09-25 03:21:28 +0000

    first advance function

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+/**
+* print_times_table - print n times table
+*
+* Description: this function prints n times table starting with 0
+*/
+void print_times_table(int n)
+{
+	int i, j, x, y, z, res;
+
+	if (n <= 15 && n >= 0)
+	{
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			res = i * j;
+			z = res % 10;
+			if (j == 0)
+			{
+				_putchar('0' + res);
+			}
+			else if (n <= 9 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + res);
+			}
+			else if (n > 9 && n <= 99)
+			{
+				x = res / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + x);
+				_putchar('0' + z);
+			}
+			else
+			{
+				y = (res / 10) % 10;
+				x = res / 100;
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + x);
+				_putchar('0' + y);
+				_putchar('0' + z);
+			}
+		}
+		_putchar('\n');
+	}
+	}
+	_putchar('\n');
+}
