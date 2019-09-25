67662c0feff22847b76fdae110e4da5401045d88
shincap8 <shincap8@gmail.com>
2019-09-25 04:02:12 +0000

    correct first advance function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,31 +17,32 @@ void print_times_table(int n)
 		{
 			res = i * j;
 			z = res % 10;
-			if (j == 0)
-				_putchar('0' + res);
-			_putchar(',');
-			_putchar(' ');
-			else if (res <= 9 && j != 0)
+			if (j != 0)
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + res);
-			}
-			else if (res > 9 && res <= 99)
-			{
-				x = res / 10;
-				_putchar(' ');
-				_putchar('0' + x);
+				if (res <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (res > 9 && res <= 99)
+				{
+					x = res / 10;
+					_putchar(' ');
+					_putchar('0' + x);
+				}
+				else
+				{
+					y = (res / 10) % 10;
+					x = res / 100;
+					_putchar('0' + x);
+					_putchar('0' + y);
+				}
 				_putchar('0' + z);
 			}
 			else
-			{
-				y = (res / 10) % 10;
-				x = res / 100;
-				_putchar('0' + x);
-				_putchar('0' + y);
-				_putchar('0' + z);
-			}
+			_putchar('0' + res);
 		}
 		_putchar('\n');
 	}
