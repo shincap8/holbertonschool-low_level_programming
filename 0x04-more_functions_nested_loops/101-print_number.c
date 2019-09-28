4a1b836216e050ab085d1573ad135004d517b533
shincap8 <shincap8@gmail.com>
2019-09-28 02:11:41 +0000

    Correct task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,38 @@
 */
 void print_number(int n)
 {
-	int x, y;
+	int p, m, c;
 
+	m = 1;
+	c = 0;
 	if (n < 0)
 	{
-		x = n * -1;
+		n = -n;
 		_putchar('-');
+	}
+	p = n;
+	if (n > 9)
+	{
+		p = n;
+		while (p > 9)
+		{
+			c++;
+			m = m * 10;
+			p = p / 10;
+		}
+		_putchar('0' + (n / m));
+		m = m / 10;
+		while (c > 0)
+		{
+			p = n;
+			p = p / m;
+			_putchar('0' + (p % 10));
+			m = m / 10;
+			c--;
+		}
 	}
 	else
 	{
-		x = n;
+		_putchar('0' + n);
 	}
-	y = x / 10;
-	if (y)
-	{
-		print_number(y);
-	}
-	_putchar('0' + (n % 10));
 }
