6d664cf8bd039cba11429496242f14b57074afcd
shincap8 <shincap8@github.com>
2019-10-01 16:09:07 +0000

    Corrrect betty on function 6 and 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -20,7 +20,7 @@ void puts2(char *str)
 		{
 			i--;
 		}
-			if(str[j] == '\0')
+			if (str[j] == '\0')
 			{
 			}
 			else
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,14 +13,14 @@ void puts_half(char *str)
 	while (str[i] != '\0')
 	{
 		i++;
-	}	
+	}
 	if ((i % 2) == 0)
 	{
 		 j = i / 2;
 	}
 	else
 	{
-		j = (i - 1) /2;
+		j = (i - 1) / 2;
 	}
 	i--;
 	for (; j <= i; j++)
