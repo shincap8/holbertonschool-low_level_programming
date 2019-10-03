5c6b811551a2aa8870498550e9d4ca1f5b191456
shincap8 <shincap8@github.com>
2019-10-03 22:22:24 +0000

    Correct output function 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 */
 char *leet(char *s)
 {
-	int i = 0, j = 0, x = 0, y = 0;
+	int i = 0, j = 0, x = 1, y = 1;
 	int M[] = {65, 69, 84, 79, 75};
 	int m[] = {97, 101, 116, 111, 108};
 	int n[] = {52, 51, 55, 48, 49};
@@ -20,7 +20,10 @@ char *leet(char *s)
 			x = M[j] - s[i];
 			y = m[j] - s[i];
 			if (x == 0 || y == 0)
+			{
 				s[i] = n[j];
+				break;
+			}
 		}
 		i++;
 	}
