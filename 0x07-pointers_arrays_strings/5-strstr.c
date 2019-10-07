4630eccf1d3dbcaf01bdbb4540177ef21f42e8e1
shincap8 <shincap8@github.com>
2019-10-07 21:24:50 +0000

    Function #5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+* *_strstr - locates a substring
+* @haystack: character haystack
+* @needle: character needle
+*
+* Description: this function locates a substring
+* Return: it returns a char
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, y = 0, x;
+
+	while (needle[j])
+	{
+		j++;
+	}
+	j--;
+	while (haystack[i])
+	{
+		for (x = 0; x <= j; )
+		{
+			if (needle[x] != haystack[i])
+				i++;
+			else if (needle[x] == haystack[i] && y == x)
+			{
+				y++;
+				i++;
+				x++;
+			}
+			if (y == j)
+			{
+				haystack = haystack + (i - j);
+				return (haystack);
+			}
+		}
+	}
+	return (0);
+}
