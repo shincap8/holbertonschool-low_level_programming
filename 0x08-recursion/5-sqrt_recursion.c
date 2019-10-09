b29333cbdc4573173fd33e2b06fc85602169a0c5
shincap8 <shincap8@gmail.com>
2019-10-09 21:27:00 +0000

    function #5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+* sqre - gets the square root of a number
+* @x: integer x
+* @y: integer y
+*
+* Description: This function gets the square of a number
+* Return: it returns an int
+*/
+int sqre(int x, int y)
+{
+	int res;
+
+	res = x / y;
+	if (y == res)
+	{
+		if ((res * res) == x)
+		{
+			return (res);
+		}
+		else
+			return (-1);
+	}
+	else
+	{
+		return (sqre(x, y - 1));
+	}
+}
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: int n
+*
+* Description: This function returns the natural square root of a number
+* Return: it returns an int
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqre(n, n));
+	}
+}
