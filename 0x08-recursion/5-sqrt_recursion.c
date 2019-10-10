7b7f2956a7955ca18b5d6c826d14076b06b1d023
shincap8 <shincap8@gmail.com>
2019-10-10 03:02:44 +0000

    Correct output function 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,28 +9,17 @@
 */
 int sqre(int x, int y)
 {
-	int res;
-
-	if (x < 0 || y > (x / 2))
+	if (y * y == x)
+	{
+		return (y);
+	}
+	if (y > (x / 2))
 	{
 		return (-1);
 	}
-	else if (x == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		res = x / y;
-		if (y == res && x % res == 0)
-		{
-			return (res);
-		}
-		else
-		{
-			return (sqre(x, y + 1));
-		}
-	}
+	y++;
+	y = sqre(x, y);
+	return (y);
 }
 /**
 * _sqrt_recursion - returns the natural square root of a number
@@ -41,5 +30,16 @@ int sqre(int x, int y)
 */
 int _sqrt_recursion(int n)
 {
-	return (sqre(n, 1));
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (sqre(n, 1));
+	}
 }
