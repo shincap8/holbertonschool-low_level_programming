0b1d84a9a57d5a00fac4cd87c6042b0ca601a3cd
shincap8 <shincap8@gmail.com>
2019-10-15 21:40:58 +0000

    Correct output function #3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,13 +13,19 @@ int **alloc_grid(int width, int height)
 	int **x;
 
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
+	}
 	x = malloc(sizeof(int *) * width);
 	for (i = 0; i < width; i++)
 	{
 		x[i] = malloc(sizeof(int) * height);
 		if (x[i] == NULL)
 		{
+			for (;i >= 0; i--)
+			{
+				free(x[i]);
+			}
 			free(x);
 			return (NULL);
 		}
