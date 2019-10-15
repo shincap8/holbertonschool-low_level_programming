bcc24bad9b56aa0fc4f7d5418c02b5af7fb8dcdb
shincap8 <shincap8@gmail.com>
2019-10-15 18:06:56 +0000

    Correct output

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,11 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	unsigned int i;
-
-	p = malloc(size);
+	unsigned int i;	
 
 	if (p == NULL || size == 0)
 		return (NULL);
+	p = malloc(size);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
