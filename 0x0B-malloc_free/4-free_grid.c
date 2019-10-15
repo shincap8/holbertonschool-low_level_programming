d57e98f7d252b39a372c00a90a517f85c7775016
shincap8 <shincap8@gmail.com>
2019-10-15 23:41:52 +0000

    Function #4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,24 +2,16 @@
 #include <stdlib.h>
 /**
  * free_grid - free a 2 dimensional gridi
- * @size: number of char in the array
- * @c: character that needs to put in the array
+ * @grid: number of char in the array
+ * @height: character that needs to put in the array
  *
- * Return: a character
  */
 void free_grid(int **grid, int height)
 {
-	char *p;
-	unsigned int i;
+	int i;
 
-	p = malloc(size);
-	if (p == NULL || size == 0)
+	for (i = 0; i < height; i++)
 	{
-		return (NULL);
+		free(gree[i]);
 	}
-	for (i = 0; i < size; i++)
-	{
-		p[i] = c;
-	}
-	return (p);
 }
