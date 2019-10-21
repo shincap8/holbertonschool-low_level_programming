840c39917150a098555fe5716d6b92522c1c769d
shincap8 <shincap8@gmail.com>
2019-10-21 12:24:23 -0500

    correct function #2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print the name of thr filr it was compiled from.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf ("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
+	return (0);
 }
