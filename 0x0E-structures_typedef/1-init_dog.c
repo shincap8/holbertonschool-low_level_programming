bc4ed3a3ccfbbf946365588618d1e7c0b12d6acb
shincap8 <shincap8@gmail.com>
2019-10-21 13:57:01 -0500

    Correct output function #1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
 * init_dog - initialize a variable of type struct dog
 * @d: structure
@@ -12,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		(*d)name = name;
+		(*d)age = age;
+		(*d)owner = owner;
 	}
 }
