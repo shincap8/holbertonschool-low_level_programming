578489303091c4121e82932e16e7d32be4d6a22d
shincap8 <shincap8@gmail.com>
2019-10-21 15:00:40 -0500

    new type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: data base for a dog
  */
-struct dog
+typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}; 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
