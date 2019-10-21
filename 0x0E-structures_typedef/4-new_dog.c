8be6dbd21c1f70fa1fc885697b7b83734adc67fd
shincap8 <shincap8@gmail.com>
2019-10-21 17:10:37 -0500

    emergency push

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+* *_strncpy - copies a string
+* @dest: character dest
+* @src: character src
+* @n: integer n
+*
+* Description: this function copies a string
+* Return: it returns a char
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = 0;
+	while (src[j])
+		j++;
+	for (i = 0; i < n; i++)
+	{
+		if (i >= j)
+			dest[i] = '\0';
+		else
+			dest[i] = src[i];
+	}
+	return (dest);
+}
+/**
 * *new_dog - Creates a new dog
 * @name: name of the dog
 * @age: age of the dog
@@ -12,6 +37,8 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0, j = 0;
+	char *newow, *newna;
 	struct dog *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
@@ -23,11 +50,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL)
 		(*new_dog).name = "";
 	else
-		(*new_dog).name = name;
+	{
+		while (name[i])
+			i++;
+		newna = malloc(i + 1);
+		if (newna == NULL)
+		{
+			free(newna);
+			free(new_dog);
+			return (NULL);
+		}
+		(*new_dog).name = *_strncpy(newna, name, i);
+	}
 	if (owner == NULL)
 		(*new_dog).owner = "";
 	else
-		(*new_dog).owner = owner;
-	(*new_dog).age = age;
+	{
+		while (owner[j])
+			j++;
+		newow = malloc(j + 1);
+		if (newow == NULL)
+		{
+			free(newow);
+			free(new_dog);
+			return (NULL);
+		}
+		(*new_dog).owner = *_strncpy(newow, owner, j);
+	}
 	return (new_dog);
 }
