e3b5d18997cd466a4b7fcf250d26c1912297486a
shincap8 <shincap8@gmail.com>
2019-10-22 00:16:10 +0000

    Function #5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
 * free_dog - frees dogs
 * @d: structure
@@ -8,8 +9,10 @@
 */
 void free_dog(dog_t *d)
 {
-	free((*d).name);
-	free((*d).age);
-	free((*d).owner);
-	free(d);
+	if (d != NULL)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
 }
