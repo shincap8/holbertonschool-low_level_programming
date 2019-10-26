b808021ff93a745f9cf732baab0a8219fcdb3f03
shincap8 <shincap8@gmail.com>
2019-10-26 12:29:51 -0500

    correct output function #2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,13 +22,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nill)");
 		else
 			printf("%s", string);
-		if (i == (n - 1))
-			printf("\n");
-		else
-		{
-			if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 				printf("%s", separator);
-		}
 	}
+	printf("\n");
 	va_end(list);
 }
