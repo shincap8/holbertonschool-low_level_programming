3b13262c1edfaa54224bed9863ed4fb05d767c56
shincap8 <shincap8@gmail.com>
2019-10-26 14:10:41 -0500

    Correct output function #3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -37,7 +37,9 @@ void print_all(const char * const format, ...)
 				break;
 		}
 		if ((format[i] == 'i' || format[i] == 'c' ||
-format[i] == 'f' || format[i] == 's') && i != (n - 1))
+format[i] == 'f' || format[i] == 's') && i != (n - 1) &&
+(format[i + 1] == 'i' || format[i + 1] == 'c' || format[i + 1] == 'f'
+|| format[i + 1] == 's'))
 			printf(", ");
 		i++;
 	}
