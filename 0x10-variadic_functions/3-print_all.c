44324670070a54a2e146a4792426a04f83c1381d
shincap8 <shincap8@gmail.com>
2019-10-26 16:41:48 -0500

    correct output function #3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
 * print_all- prints anything
 * @format: format
@@ -10,36 +11,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	int i = 0, n = 0;
+	int i = 0;
 	char *s;
 
-	while (format[n] == && format)
-		n++;
-	va_start(list, format);
-	while (i < n)
+	while (format && format[i])
 	{
-		switch (format[i])
+		va_start(list, format);
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(list, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(list, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(list, double));
-				break;
-			case 's':
-				s = va_arg(list, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s", s);
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(list, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(list, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(list, double));
+					break;
+				case 's':
+					s = va_arg(list, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s", s);
+					break;
+			}
+			if ((format[i] == 'i' || format[i] == 'c' ||
+format[i] == 'f' || format[i] == 's') && format[i + 1] != '\0')
+				printf(", ");
+			i++;
 		}
-		if ((format[i] == 'i' || format[i] == 'c' ||
-format[i] == 'f' || format[i] == 's') && i != (n - 1))
-			printf(", ");
-		i++;
 	}
 	printf("\n");
 	va_end(list);
