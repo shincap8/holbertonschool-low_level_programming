f38480548be619e12b835c6aefa55ff7b7c45fca
shincap8 <shincap8@gmail.com>
2019-10-27 16:48:24 +0000

    Correct requirement function 100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,12 @@
+section .data
+	text db 'Hello, Holberton',0xa
 section .text
-	global _start
-
-_start:
-	mov edx,17
-	mov ecx,text
+	global main 
+main:
 	mov ebx,1
+	mov ecx,text
+	mov edx,17
 	mov eax,4
 	int 0x80
 	mov eax,0
 	int 0x80
-
-section.data
-	text db 'Hello, Holberton',0xa
