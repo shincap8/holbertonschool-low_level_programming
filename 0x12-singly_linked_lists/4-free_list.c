0515c6ea49f1e4482e2be5ab54c858a45f8e24db
shincap8 <shincap8@gmail.com>
2019-10-31 22:43:38 +0000

    Add Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* free_list- free the list
+* @head: pointer to the first element
+*
+* Description: this function frees the list
+*/
+void free_list(list_t *head)
+{
+	list_t *aux = head;
+
+	if (head == NULL)
+		free(head);
+	while (aux->next)
+	{
+		free(head->str);
+		free(head);
+		head = aux->next;
+		aux = aux->next;
+	}
+	free(head->str);
+	free(head);
+}
