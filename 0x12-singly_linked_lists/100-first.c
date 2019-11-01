62182e2e74d42fcdf30f14b4d7db9258a1932f7d
shincap8 <shincap8@gmail.com>
2019-11-01 00:48:12 +0000

    Correct betty advance task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
-* startupfun- prints all elements of a list
-* @h: pointer to the first element
+* startupfun- execute before main
 *
-* Description: this function prints all elements of a list
-* Return: the number of nodes in the list
+* Description: this function execute before main
 */
-void startupfun (void) __attribute__ ((constructor));
-void startupfun (void)
+void startupfun(void) __attribute__ ((constructor));
+void startupfun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
 }
