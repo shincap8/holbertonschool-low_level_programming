af1ccb4880ff1752fb5943efcd90311b7950c899
shincap8 <shincap8@gmail.com>
2019-11-01 00:53:17 +0000

    Correct betty advance task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+void startupfun(void) __attribute__ ((constructor));
 /**
 * startupfun- execute before main
 *
 * Description: this function execute before main
 */
-void startupfun(void) __attribute__ ((constructor));
 void startupfun(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
