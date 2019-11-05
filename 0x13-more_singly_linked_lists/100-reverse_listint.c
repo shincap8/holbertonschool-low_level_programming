942b7afb9abff70d8b250b4e0facd9f065d466c9
shincap8 <shincap8@gmail.com>
2019-11-05 19:29:58 +0000

    Correct betty advance task

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,28 +3,28 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
-* free_listint2- free the list
+* reverse_listint- free the list
 * @head: pointer to the first element
 *
-* Description: this function frees the list
+* Description: this function reverse the list
+* Return: pointer to the new first node
 */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *aux = *head, *follow;
 
 	if (head == NULL)
-		return (*head);
+		return (NULL);
 	*head = (*head)->next;
 	aux->next = NULL;
 	follow = *head;
 	while (follow->next)
 	{
-			*head = (*head)->next;
-			follow->next = aux;
-			aux = follow;
-			follow = *head;
-		
+		*head = (*head)->next;
+		follow->next = aux;
+		aux = follow;
+		follow = *head;
 	}
 	follow->next = aux;
-	return (*head);
+	return (follow);
 }
