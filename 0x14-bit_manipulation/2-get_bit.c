dfa6316011cee22079dbd8b36aba4fb0774370c2
shincap8 <shincap8@gmail.com>
2019-11-07 18:35:47 +0000

    Correct output

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* get_bin - function that prints a variable type b
+* get_bit - gets the bit at a given index
 * @n: number
 * @index: location of the number returned
 * Return: the number in the index
@@ -10,6 +10,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (n == 0 && index == 0)
+		return (0);
+	if (size_of(n) >= 8)
+		return (-1);
 	for (i = 0; i < index; i++)
 	{
 		n = n >> 1;
