02be02a34fffaec0212e5be3e6cd8c488900ff12
shincap8 <shincap8@gmail.com>
2019-11-13 00:50:47 +0000

    correct output task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int ac, char **av)
 	char *copy[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: %s %s %s\n", av[0], av[1], av[2]), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd = open(av[1], O_RDONLY, 0600);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
