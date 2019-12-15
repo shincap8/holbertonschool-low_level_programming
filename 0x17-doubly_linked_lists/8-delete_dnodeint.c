98bc1c4dbb42c6b603dd0cb18fd06e40269d1a09
shincap8 <shincap8@gmail.com>
2019-12-15 20:44:42 +0000

    second advance task written in emacs

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 1;
 	dlistint_t *remove, *tmp = *head;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	if (index == 0)
 	{
