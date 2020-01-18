dd636fd2c22bc7cc0a2dcb9d5b71d9aebc9d4858
shincap8 <shincap8@gmail.com>
2020-01-18 19:17:41 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,16 +10,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
-	unsigned const char *node_key;
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	node_key = strdup(key);
-	index = key_index(node_key, ht->size);
-	if (ht->array[index] == 0)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
 	{
 		new_node->next = NULL;
 		ht->array[index] = new_node;
