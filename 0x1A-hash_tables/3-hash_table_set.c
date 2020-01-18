2ca10a6e1c2a4fee1bf03911ac093068da7939ce
shincap8 <shincap8@gmail.com>
2020-01-18 20:10:31 +0000

    Adding condition if key is empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (key == "" || key == NULL)
+	if (key[0] == '\0')
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
