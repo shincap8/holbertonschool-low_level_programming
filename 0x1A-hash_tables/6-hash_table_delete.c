126ec1a7c12ff76befeef4f3aace75d817ceef2b
shincap8 <shincap8@gmail.com>
2020-01-19 18:22:50 +0000

    Task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+* hash_table_delete - deletes the hash table
+* @ht: pointer to hash table
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *aux;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	if (ht != NULL && ht->size != 0)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				aux = ht->array[i];
+				while (aux)
+				{
+					free(aux->value);
+					free(aux->key);
+					free(ht->array[i]);
+					aux = aux->next;
+				}
+			}
+			free(ht->array[i]);
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
